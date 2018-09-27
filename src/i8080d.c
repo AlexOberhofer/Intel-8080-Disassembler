17384f6415470d3c271ce7eee40b5f3e0f21c042
Alex <AlexOberhofer@nc.rr.com>
2018-09-26 20:51:34 -0400

    Added first handful of codes and added more 8080 documentation

--- src/i8080d.c
+++ src/i8080d.c
@@ -4,13 +4,43 @@
 int disassemble(unsigned char *buffer, int pc){
 
     int op_size = 1;
+
+    /**
+     * Notes:
+     * low bit opcode[1]
+     * high bit opcode[2]
+     */
     unsigned char *opcode = &buffer[pc];
     printf("%04x ", pc);
 
     switch(*opcode){
         case 0x00: printf("NOP"); break;
-        case 0x01: printf("LXI      B, #$%02x%02x", opcode[2], opcode[1]);
+        case 0x01: printf("LXI        B, #%02x #%02x", opcode[2], opcode[1]);
         op_size = 3; break;
+        case 0x02: printf("STAX       B"); break;
+        case 0x03: printf("INX        B"); break;
+        case 0x04: printf("INR        B"); break;
+        case 0x05: printf("DCR        B"); break;
+        case 0x06: printf("MVI        B, #%02x", opcode[1]); op_size = 2; break;
+        case 0x07: printf("RLC"); break;
+        case 0x09: printf("DAD        B"); break;
+        case 0x0a: printf("LDAX       B"); break;
+        case 0x0b: printf("DCX        B"); break;
+        case 0x0c: printf("INR        C"); break;
+        case 0x0d: printf("DCR        C"); break;
+        case 0x0e: printf("MVI        C, #%02x", opcode[1]); op_size = 2; break;
+        case 0x0f: printf("RRC"); break;
+        case 0x10: printf("NOP");
+        case 0x11: printf("LXI        D, #%02x #%02x", opcode[2], opcode[1]);
+                    op_size = 3; break;
+        case 0x12: printf("STAX       D"); break;
+        case 0x13: printf("INX        D"); break;
+        case 0x14: printf("INR        D"); break;
+        case 0x15: printf("DCR        D"); break;
+        case 0x16: printf("MVI        D, #%02x", opcode[1]); op_size = 2; break;
+        case 0x17: printf("RAL"); break;
+        case 0x19: printf("DAD"); break;
+        default: printf("NOP"); break;
     }
 
     return op_size;
